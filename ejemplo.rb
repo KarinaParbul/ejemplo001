d70d9a12ecd9d7a53588d639f7cfb3a339410680
KarinaParbul <l19290638@cdguzman.tecnm.mx>
2024-08-29 16:45:20 +0000

    actualizacion

---
+++ ejemplo.rb
@@ -0,0 +1,19 @@
+class Padre 
+    def initialize (nombre)
+        @nombre=nombre
+    end
+    def say_hello
+        "Hola #{@nombre} desde  #{self}"
+    end
+end
+
+class Hijo <Padre
+end
+
+saluda1=Padre.new("Darth")
+puts saluda1.say_hello
+
+saluda2= Hijo.new("Anakin")
+puts saluda2.say_hello
+
+
